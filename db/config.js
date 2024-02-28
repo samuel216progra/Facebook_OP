0ea04aac96ee204dc8dad5141e6a88f16a7da93e
samuer216progra <srodriguez-2022165@kinal.edu.gt>
2024-02-28 00:50:11 -0600

    se levanto el servidor

---
+++ db/config.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+
+const dbConnection = async () => {
+    try{
+        await mongoose.connect(process.env.MONGODB_CNN,{});
+        console.log('Base de datos conectada');
+
+    }catch(error){
+        throw new Error('Error al conectar a la base de datos')
+    }
+
+}
+
+module.exports = {
+    dbConnection
+}
---
+++ models/server.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const cors = require ('cors');
+const { dbConnection } = require('../db/config');
+
+class Server{
+
+    constructor(){
+        this.app = express();
+        this.port = process.env.PORT;
+
+        this.conectarDB();
+        this.middlewares();
+        
+    }
+
+    async conectarDB(){
+        await dbConnection();
+    }
+
+    middlewares(){
+        this.app.use(express.static('public'));
+        this.app.use(cors());
+        this.app.use(express.json());
+    }
+
+    listen(){
+        this.app.listen(this.port,() => {
+
+            console.log('Servidor ejecutandose y escuchandose en el puerto :', this.port );
+        });
+    }
+}
+
+module.exports = Server;
